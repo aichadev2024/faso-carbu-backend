0cfbb373ac5507a4a836664e473bcc8220ccc8bf
diarr <aicha.dev2024@gmail.com>
2025-08-11 10:12:43 +0000

    Correction / ajout dans TicketRepository

--- src/main/java/com/fasocarbu/fasocarbu/repositories/TicketRepository.java
+++ src/main/java/com/fasocarbu/fasocarbu/repositories/TicketRepository.java
@@ -1,11 +1,15 @@
 package com.fasocarbu.fasocarbu.repositories;
 
+import com.fasocarbu.fasocarbu.enums.StatutTicket;
 import com.fasocarbu.fasocarbu.models.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
+import java.util.List;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
     Optional<Ticket> findByCodeQr(String codeQr);
+    List<Ticket> findByStatut(StatutTicket statut);
+
 
 }
